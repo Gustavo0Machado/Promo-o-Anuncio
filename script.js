3e83d54b370a8d82c6b38c39df52a1a6dcf280ae
GUSTAVO MACHADO <22001772-GUSTAVOMACHAD29@users.noreply.replit.com>
2024-04-04 01:28:32 +0000

    03/04/2024

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const botoes = document.querySelectorAll('.botao');
+const botoes = document.querySelectorAll(".botao");
 
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function() {
-    for(let k=0; k < botoes. legth; k++ )i {
-      botoes[k].classList.romove('ativo');
-    }
-    botoes[i].classList.add('ativo');
-  }
+for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
+
+for(let j=0;j<botoes.length;j++) {
+
+  botoes[j].classList.remove("ativo");
+
+}
+
+botoes[i].classList.add("ativo");
+
+}
+
 }
